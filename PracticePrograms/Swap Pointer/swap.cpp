f9803ebfb22cb2c01b503989f7b0f1315b8ef487
marnggo <marionegalman18@gmail.com>
2025-08-18 01:02:47 +0800

    modified swap program

--- PracticePrograms/Swap Pointer/swap.cpp
+++ PracticePrograms/Swap Pointer/swap.cpp
@@ -12,18 +12,25 @@ void swap(int &x, int &y) {
 
 int main() {
     int var1, var2;
+    int exitFlag = 0;
 
-    std::cout << "---------------- Swapper ------------------" << std::endl;
+    while(exitFlag == 0) {
+        
+        std::cout << "---------------- Swapper ------------------" << std::endl;
 
-    std::cout << "enter variable 1: " << std::endl;
-    std::cin >> var1;
+        std::cout << "enter variable 1: " << std::endl;
+        std::cin >> var1;
 
-    std::cout << "enter variable 2: " << std::endl;
-    std::cin >> var2;
+        std::cout << "enter variable 2: " << std::endl;
+        std::cin >> var2;
 
-    swap(var1, var2);
+        if(var1 == 0 && var2 == 0)
+            exitFlag = 1;
 
-    std::cout << "variable 1: " << var1 << std::endl << "variable 2: " << var2 << std::endl;
+        swap(var1, var2);
 
+        std::cout << "variable 1: " << var1 << std::endl << "variable 2: " << var2 << std::endl;
+    }
+    
     return 0;
 }
